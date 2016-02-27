bfa8a460f83b42a78922c7f6fd14db938249022b
ksasao <k.sasao@gmail.com>
2016-02-27 20:39:11 +0900

    Mac/Linuxでビルドできなくなっていたのを修正(ビルド後イベント削除)

---
+++ src/Gochiusearch/copytools.bat
@@ -0,0 +1,15 @@
+rem ‚·‚×‚ÄƒŠƒrƒ‹ƒh‚µ‚½ŒãÀs‚·‚é
+SET OUTPUT=..\..\Tools
+mkdir %OUTPUT%
+copy /y bin\debug\readme.txt %OUTPUT%
+copy /y bin\debug\Gochiusearch.exe %OUTPUT%
+copy /y bin\debug\index.db %OUTPUT%
+copy /y bin\debug\index.txt %OUTPUT%
+
+copy /y ..\CreateIndex\bin\debug\CreateIndex.exe %OUTPUT%
+copy /y ..\CreateIndex\Script\avi2jpg.bat %OUTPUT%
+copy /y ..\CreateIndex\Script\repeat.bat %OUTPUT%
+copy /y ..\CreateIndex\Script\readme-index.txt %OUTPUT%
+copy /y ..\CreateDb\bin\debug\CreateDb.exe %OUTPUT%
+
+copy /y ..\ImageSearchEngine\bin\debug\ImageSearchEngine.dll %OUTPUT%
